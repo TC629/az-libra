2ae4b79dfbd6982418f2f412af73f3be27201e3d
Adolfo García <adolfo.garcia.cr@gmail.com>
2014-06-03 17:12:30 -0600

    Habilitar sunxi-tools.

--- utils/clone-repos.sh
+++ utils/clone-repos.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/linux-sunxi/u-boot-sunxi.git
 git clone -b sunxi-3.4 https://github.com/linux-sunxi/linux-sunxi.git
-#git clone https://github.com/linux-sunxi/sunxi-tools.git
+git clone https://github.com/linux-sunxi/sunxi-tools.git
 #git clone https://github.com/linux-sunxi/sunxi-boards.git
